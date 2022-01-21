c8f579b236cfe51f7c447cb0f7bc94dec041f35d
CLAW <christopher.lawn@yahoo.com>
2022-01-20 18:45:48 -0800

    set up htmlRoutes

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,14 @@
+const { fstat } = require('fs');
+const path = require('path');
+const router = require('express').Router();
+
+router.get('/api/notes', (req, res) => {
+    fs.readfile
+    res.json
+});
+
+router.post('/', (req, res) => {
+
+});
+
+module.exports = router;

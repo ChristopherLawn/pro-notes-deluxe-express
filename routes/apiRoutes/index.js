3ce851d7c776224aac987cc2eb3d550cdf38cec5
CLAW <christopher.lawn@yahoo.com>
2022-01-21 19:33:08 -0800

    updated apiRoutes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,14 +1,46 @@
-const { fstat } = require('fs');
+const fs = require('fs');
 const path = require('path');
 const router = require('express').Router();
+const { v4: uuidv4 } = require('uuid')
+const notes= require("../../db/db.json");
 
-router.get('/api/notes', (req, res) => {
-    fs.readfile
-    res.json
+router.get("/notes", (req, res) => {
+    fs.readFile((path.join(__dirname, '../../db/db.json')), 'utf8', (err, data) => {
+        if (err) throw err;
+        res.json(JSON.parse(data));
+    });
 });
 
-router.post('/', (req, res) => {
-
+router.post("/notes", (req, res) => {
+    const newNote = req.body
+    const newId = uuidv4()
+    newNote.id = newId
+    fs.readFile((path.join(__dirname, '../../db/db.json')), 'utf8', (err, data) => {
+        if (err) throw err;
+        const currentNotes = JSON.parse(data);
+        currentNotes.push(newNote);
+        console.log(currentNotes);
+        fs.writeFile((path.join(__dirname, '../../db/db.json')), JSON.stringify(currentNotes), (err) =>{
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("File written successfully");
+                return res.json(newNote)
+        }});
+    });
 });
+
+router.delete("/notes/:id", (req, res) => {
+    fs.readFile((path.join(__dirname, '../../db/db.json')), 'utf8', (err, data) => {
+        if (err) throw err;
+        const currentNotes = JSON.parse(data);
+        const noteId = req.params.id;
+        // const compareFunction = (currentNotes) => {
+        //     if (noteId )
+        // }
+        const newNotesArray = currentNotes.filter(compareFunction)
+    });
+
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const path = require('path');
-// const apiRoutes = require('./routes/apiRoutes');
+const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
 const PORT = process.env.PORT || 3001;
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
-// app.use('/api', apiRoutes);
+app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 

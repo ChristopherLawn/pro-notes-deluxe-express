55abf029a19f1d2fd70ac550ce2356f97c6406e6
CLAW <christopher.lawn@yahoo.com>
2022-01-21 20:22:01 -0800

    completed all apiRoutes functions for adding and deleting notes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 const path = require('path');
 const router = require('express').Router();
 const { v4: uuidv4 } = require('uuid')
-const notes= require("../../db/db.json");
 
 router.get("/notes", (req, res) => {
     fs.readFile((path.join(__dirname, '../../db/db.json')), 'utf8', (err, data) => {
@@ -19,14 +18,13 @@ router.post("/notes", (req, res) => {
         if (err) throw err;
         const currentNotes = JSON.parse(data);
         currentNotes.push(newNote);
-        console.log(currentNotes);
-        fs.writeFile((path.join(__dirname, '../../db/db.json')), JSON.stringify(currentNotes), (err) =>{
+        fs.writeFile((path.join(__dirname, '../../db/db.json')), JSON.stringify(currentNotes), (err) => {
             if (err) {
                 console.log(err);
             } else {
-                console.log("File written successfully");
                 return res.json(newNote)
-        }});
+            }
+        });
     });
 });
 
@@ -35,12 +33,16 @@ router.delete("/notes/:id", (req, res) => {
         if (err) throw err;
         const currentNotes = JSON.parse(data);
         const noteId = req.params.id;
-        // const compareFunction = (currentNotes) => {
-        //     if (noteId )
-        // }
-        const newNotesArray = currentNotes.filter(compareFunction)
-    });
 
-})
+        const newNotesArray = currentNotes.filter((note => note.id !== noteId));
+        fs.writeFile((path.join(__dirname, '../../db/db.json')), JSON.stringify(newNotesArray), (err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                return res.json(newNotesArray)
+            }
+        });
+    })
+});
 
 module.exports = router;
